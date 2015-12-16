d45f5c7c060dbc6456d24a6c7c17e34b973306a0
Senpai Suhyun <Senpai@Senpais-MacBook-Pro.local>
2015-12-16 00:21:27 -0500

    Added api keys and addresses.

--- app/src/main/java/com/bgirlogic/movies/api/Constants.java
+++ app/src/main/java/com/bgirlogic/movies/api/Constants.java
@@ -5,6 +5,7 @@ package com.bgirlogic.movies.api;
  */
 public class Constants {
 
-    public static final String API_ADDRESS="";
-    public static final String API_ADDRESS_ENDPOINT="";
+    public static final String API_ADDRESS = "http://api.themoviedb.org";
+    public static final String API_ADDRESS_ENDPOINT = "";
+    public static final String API_KEY = "8df5a981e3421ec1fe8fb26bc13e368b";
 }
