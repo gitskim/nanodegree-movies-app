8894de506dcbc90f24924faa8ea48e81685f9c05
Senpai Suhyun <logicdevildotcom@gmail.com>
2016-01-24 10:37:04 -0500

    Done writing the content provider

--- app/src/main/java/com/bgirlogic/movies/data/MoviesProvider.java
+++ app/src/main/java/com/bgirlogic/movies/data/MoviesProvider.java
@@ -5,14 +5,18 @@ import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by kimsuh on 1/19/16.
  */
 public class MoviesProvider extends ContentProvider {
+
+    private static final String TAG = MoviesProvider.class.getSimpleName();
     private static final UriMatcher sUriMatcher = buildUriMatcher();
     private MoviesDBHelper mOpenHelper;
 
@@ -58,12 +62,12 @@ public class MoviesProvider extends ContentProvider {
                 return retCursor;
             }
             //individual movie based on id selected
-            case MOVIE_WITH_ID:{
+            case MOVIE_WITH_ID: {
                 retCursor = mOpenHelper.getReadableDatabase().query(
                         MoviesContract.MovieEntry.TABLE_MOVIES,
                         projection,
                         MoviesContract.MovieEntry._ID + " = ?",
-                        new String[] {String.valueOf(ContentUris.parseId(uri))},
+                        new String[]{String.valueOf(ContentUris.parseId(uri))},
                         null,
                         null,
                         sortOrder);
@@ -107,7 +111,7 @@ public class MoviesProvider extends ContentProvider {
                 } else {
                     throw new android.database.SQLException("Failed to insert row into: " + uri);
                 }
-                
+
                 break;
             }
 
@@ -121,11 +125,110 @@ public class MoviesProvider extends ContentProvider {
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int numDeleted;
+        switch (match) {
+            case MOVIE:
+                numDeleted = db.delete(MoviesContract.MovieEntry.TABLE_MOVIES,
+                        selection, selectionArgs);
+                //reset _ID
+                db.execSQL("DELETE FROM SQLITE SEQUENCE WHERE NAME = '"
+                        + MoviesContract.MovieEntry.TABLE_MOVIES + "'");
+                break;
+            case MOVIE_WITH_ID:
+                numDeleted = db.delete(MoviesContract.MovieEntry.TABLE_MOVIES,
+                        MoviesContract.MovieEntry._ID + " = ?",
+                        new String[]{String.valueOf(ContentUris.parseId(uri))});
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        return numDeleted;
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        int numUpdated = 0;
+
+        if (values == null) {
+            throw new IllegalArgumentException("Cannot have null content values");
+        }
+
+        switch (sUriMatcher.match(uri)) {
+            case MOVIE: {
+                numUpdated = db.update(MoviesContract.MovieEntry.TABLE_MOVIES,
+                        values,
+                        selection,
+                        selectionArgs);
+                break;
+            }
+            case MOVIE_WITH_ID: {
+                numUpdated = db.update(MoviesContract.MovieEntry.TABLE_MOVIES,
+                        values,
+                        MoviesContract.MovieEntry._ID + " = ?",
+                        new String[]{String.valueOf(ContentUris.parseId(uri))});
+            }
+            default: {
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+            }
+        }
+
+        if (numUpdated > 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numUpdated;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case MOVIE:
+                // allows for multiple transactions
+                db.beginTransaction();
+
+                // keep track of successful inserts
+                int numInserted = 0;
+                try {
+                    for (ContentValues value : values) {
+                        if (value == null) {
+                            throw new IllegalArgumentException("Cannot have null content values");
+                        }
+                        long _id = -1;
+                        try {
+                            _id = db.insertOrThrow(MoviesContract.MovieEntry.TABLE_MOVIES,
+                                    null, value);
+                        } catch (SQLiteConstraintException e) {
+                            Log.w(TAG, "Attempting to insert the movie with this following poster path" +
+                                    value.getAsString(
+                                            MoviesContract.MovieEntry.COLUMN_POSTER_PATH)
+                                    + " but value is already in database.");
+                        }
+                        if (_id != -1) {
+                            numInserted++;
+                        }
+                    }
+                    if (numInserted > 0) {
+                        // If no errors, declare a successful transaction.
+                        // database will not populate if this is not called
+                        db.setTransactionSuccessful();
+                    }
+                } finally {
+                    // all transactions occur at once
+                    db.endTransaction();
+                }
+                if (numInserted > 0) {
+                    // if there was successful insertion, notify the content resolver that there
+                    // was a change
+                    getContext().getContentResolver().notifyChange(uri, null);
+                }
+                return numInserted;
+            default:
+                return super.bulkInsert(uri, values);
+        }
     }
 }
